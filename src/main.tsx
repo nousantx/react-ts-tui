2ac498b762d274ae921a5000fa20771f780b21bf
nousantx <nousantx@gmail.com>
2024-02-03 21:54:58 +0700

    Fix: Update some code

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import "tenoxui/dist/css/lib.css";
+import "tenoxui/dist/css/tui.css";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
