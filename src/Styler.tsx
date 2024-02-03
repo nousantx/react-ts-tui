98e240b86349f19237125b468f0f0552699ac77c
NOuSantx <112807896+nousantx@users.noreply.github.com>
2024-02-04 06:13:08 +0700

    Fix: Add comment

--- src/Styler.tsx
+++ src/Styler.tsx
@@ -4,29 +4,35 @@ import tenoxui, { defineProps, makeStyles, moreColor } from "tenoxui";
 const Styler: React.FC = () => {
   useEffect(() => {
     defineProps({
+      // Custom type and property that can be used
       dis: ["display"],
-      bdr: ["border"],
-      bc: ["borderColor"],
-      all: ["all"],
-      "ob-fit": ["objectFit"],
-      "bd-bottom": ["borderBottom"],
+      // You can use both camelCase or kebab-case
+      "fx-par": ["alignItems", "justify-content"],
     });
 
     makeStyles({
+      // make style for body here
       body: "bg-[neutral-100] tc-[neutral-900]",
-      ".w-full,.w-mx": "w-100%",
-      ".h-full,.h-mx": "h-100%",
+      // define utility class you will use
+      ".w-full, .w-mx": "w-100%",
+      ".h-full, .h-mx": "h-100%",
       ".flex": "dis-flex",
-      ".fx-ctr,.flex-center": "dis-flex flex-parent-center",
+      ".fx-ctr, .flex-center": "dis-flex flex-parent-center",
       ".fd-col": "fd-column",
+      // Re-usable position property
       ".fixed": "post-fixed",
+      ".absolute": "post-absolute",
+      ".relative": "post-relative",
     });
 
+    // add more color functionality hex, rgb, and rgba color
     moreColor();
+    // initiate TenoxUI
     tenoxui();
   }, []);
 
   return null;
 };
 
+// Export styler
 export default Styler;
