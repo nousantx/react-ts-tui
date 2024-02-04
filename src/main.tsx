6afd11356e53d9e9a2a9a5c1d9c60c3106f3dbb5
NOuSantx <112807896+nousantx@users.noreply.github.com>
2024-02-04 09:23:12 +0700

    Fix: Remove white space

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import './index.css'
 import "tenoxui/dist/css/lib.css";
 import "tenoxui/dist/css/tui.css";
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
